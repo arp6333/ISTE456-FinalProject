3e24c24f14d18751fe2650e96f07eb95bba082ab
Ellie <ellie.parobek@gmail.com>
2020-11-29 19:25:23 -0500

    Front end fragments

--- app/src/main/java/com/example/finalproject/FirstFragment.kt
+++ app/src/main/java/com/example/finalproject/FirstFragment.kt
@@ -1,21 +1,27 @@
 package com.example.finalproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private var curDate: String = ""
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -24,8 +30,19 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        val c = Calendar.getInstance().time
+        val df = SimpleDateFormat("MM / dd / yyyy", Locale.getDefault())
+        curDate = df.format(c)
+
+        button_first.setOnClickListener {
+            val bundle = bundleOf("date" to curDate)
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, bundle)
+        }
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            // month starts at 0 so we add 1 to it
+            var actualMonth = month + 1
+            curDate = "$actualMonth / $dayOfMonth / $year"
         }
     }
 }
